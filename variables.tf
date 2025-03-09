debf5a1f8160c8d5670b082c2f42b5160078a600
Jason Elliott <54967024+cloudysideup@users.noreply.github.com>
2025-03-09 14:03:33 -0500

    var update
    add CIDR's as env var in repo settings

--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "region_with_availability_zone" {
   description = "Region with availability zone"
   type        = string
+  default     = "#{AZ_REGION}#"
 }
 
 variable "hub_cidr" {
   description = "CIDR for the hub vnet"
   type        = string
+  default     = "#{AZ_HUB_ADDRESS_SPACE}#"
 }
 
 variable "gateway_subnet_cidr" {
   description = "CIDR for the hub vnet gateway subnet"
   type        = string
+  default     = "#{AZ_HUB_GATEWAY_SUBNET}#"
 }
 
 variable "private_endpoint_subnet_cidr" {
   description = "CIDR for the hub vnet private endpoint subnet"
   type        = string
+  default     = "#{AZ_HUB_PRIVATE_ENDPOINT_SUBNET}#"
 }
 
 variable "firewall_subnet_cidr" {
   description = "CIDR for the hub vnet firewall subnet"
   type        = string
+  default     = "#{AZ_HUB_FIREWALL_SUBNET}#"
 }
