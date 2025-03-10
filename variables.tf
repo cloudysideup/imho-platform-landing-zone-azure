a7d1781ee681c90ec754eeb1829d857b28e0dbf3
Jason Elliott <54967024+cloudysideup@users.noreply.github.com>
2025-03-10 10:06:14 -0500

    fix types
    CIDR's are single values but expected as a list of strings as per AzureRM provider

--- variables.tf
+++ variables.tf
@@ -6,38 +6,38 @@ variable "region_with_availability_zone" {
 
 variable "hub_cidr" {
   description = "CIDR for the hub vnet"
-  type        = string
-  default     = "#{AZ_HUB_ADDRESS_SPACE}#"
+  type        = list(string)
+  default     = ["#{AZ_HUB_ADDRESS_SPACE}#"]
 }
 
 variable "gateway_subnet_cidr" {
   description = "CIDR for the hub vnet gateway subnet"
-  type        = string
-  default     = "#{AZ_HUB_GATEWAY_SUBNET}#"
+  type        = list(string)
+  default     = ["#{AZ_HUB_GATEWAY_SUBNET}#"]
 }
 
 variable "private_endpoint_subnet_cidr" {
   description = "CIDR for the hub vnet private endpoint subnet"
-  type        = string
-  default     = "#{AZ_HUB_PRIVATE_ENDPOINT_SUBNET}#"
+  type        = list(string)
+  default     = ["#{AZ_HUB_PRIVATE_ENDPOINT_SUBNET}#"]
 }
 
 variable "firewall_subnet_cidr" {
   description = "CIDR for the hub vnet firewall subnet"
-  type        = string
-  default     = "#{AZ_HUB_FIREWALL_SUBNET}#"
+  type        = list(string)
+  default     = ["#{AZ_HUB_FIREWALL_SUBNET}#"]
 }
 
 variable "private_dns_in_subnet_cidr" {
   description = "CIDR for the hub vnet private DNS in subnet"
-  type        = string
-  default     = "#{AZ_HUB_PRIVATE_DNS_IN_SUBNET}#"
+  type        = list(string)
+  default     = ["#{AZ_HUB_PRIVATE_DNS_IN_SUBNET}#"]
 }
 
 variable "private_dns_out_subnet_cidr" {
   description = "CIDR for the hub vnet private DNS out subnet"
-  type        = string
-  default     = "#{AZ_HUB_PRIVATE_DNS_OUT_SUBNET}#"
+  type        = list(string)
+  default     = ["#{AZ_HUB_PRIVATE_DNS_OUT_SUBNET}#"]
 }
 
 variable "dns_zones" {
