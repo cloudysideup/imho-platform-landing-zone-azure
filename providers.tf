71f7835802562b87ecd4018aa93edc1bae725a64
Jason Elliott <54967024+cloudysideup@users.noreply.github.com>
2025-03-11 10:43:12 -0500

    OIDC config

--- providers.tf
+++ providers.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {
-  client_id       = "#{TF_AZ_SPN_CLIENT_ID}#"
-  subscription_id = "#{TF_AZ_SUBSCRIPTION_ID}#"
   tenant_id       = "#{TF_AZ_TENANT_ID}#"
+  subscription_id = "#{TF_AZ_SUBSCRIPTION_ID}#"
   features {}
 }
